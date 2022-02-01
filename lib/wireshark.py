e0d9f6fc02297c4ec36d42803c1310767eaee9d1
wizard <wizard@kali>
2022-02-01 10:38:04 -0500

    Add create_ functions to Wireshark class. 29.07pps, 5k: 2:52

--- lib/wireshark.py
+++ lib/wireshark.py
@@ -43,120 +43,125 @@ class Wireshark:
             ssid = get_ssid(packet)
 
             # Create/merge nodes for the IP addresses
-            create_ip(neo4j, ip_src)
-            create_ip(neo4j, ip_dst)
+            self.create_ip(neo4j, ip_src)
+            self.create_ip(neo4j, ip_dst)
 
             # Create/merge nodes for the MAC addresses
-            create_mac(neo4j, mac_src, oui=oui_src)
-            create_mac(neo4j, mac_dst, oui=oui_dst)
-            create_mac(neo4j, mac_tra)
-            create_mac(neo4j, mac_rec)
+            self.create_mac(neo4j, mac_src, oui=oui_src)
+            self.create_mac(neo4j, mac_dst, oui=oui_dst)
+            self.create_mac(neo4j, mac_tra)
+            self.create_mac(neo4j, mac_rec)
 
             # Assign the IP addresses to the MAC addresses
-            if mac_src not in self.ignore and ip_src is not None:
-                neo4j.new_relationship(ip_src, mac_src, 'ASSIGNED')
-            if mac_dst not in self.ignore and ip_dst is not None:
-                neo4j.new_relationship(ip_dst, mac_dst, 'ASSIGNED')
+            self.create_mac_assignment(neo4j, ip_src, mac_src)
+            self.create_mac_assignment(neo4j, ip_dst, mac_dst)
 
             # Create or update the connection relationship for the packet
             if None not in (ip_src, ip_dst):
                 # Create a connection between IP addresses
-                create_connection(neo4j, ip_src, ip_dst, port_dst, proto, time, length, service, service_layer)
+                self.create_connection(neo4j, ip_src, ip_dst, port_dst, proto, time, length, service, service_layer)
             elif None not in (mac_src, mac_dst):
                 # Create a connection between MAC addresses
-                create_connection_mac(neo4j, mac_src, mac_dst, proto, time, length, service, service_layer)
+                self.create_connection_mac(neo4j, mac_src, mac_dst, proto, time, length, service, service_layer)
             if None not in (mac_src, mac_dst, mac_tra, mac_rec):
                 # Create a connection between MAC addresses
                 # This is for wlan frames that have ra and ta
                 # We are connecting the sender to transmitter, receiver to destination
-                create_connection_mac(neo4j, mac_src, mac_tra, proto, time, length, service, service_layer)
-                create_connection_mac(neo4j, mac_rec, mac_dst, proto, time, length, service, service_layer)
+                self.create_connection_mac(neo4j, mac_src, mac_tra, proto, time, length, service, service_layer)
+                self.create_connection_mac(neo4j, mac_rec, mac_dst, proto, time, length, service, service_layer)
 
-            if ssid is not None:
-                neo4j.new_node('SSID', f'{{name: "{ssid}"}}')
-                neo4j.new_relationship(mac_src, ssid, 'ADVERTISES')
+            self.create_ssid(neo4j, ssid, mac_src)
 
             debug_count += 1
 
-def create_connection(neo4j, ip_src, ip_dst, port_dst, proto, time, length, service, service_layer):
-    if port_dst is None:
-        port_dst = -1
+    '''
+    Deprecated, creates too many edges which probably aren't useful anyway
+    '''
+    def create_port_relationship(self, neo4j, ip_src, ip_dst, port_src, port_dst, proto, time, length):
+        props = '{'
+        props += f'srcport: {port_src}, '
+        props += f'dstport: {port_dst}, '
+        props += f'protocol: "{proto}", '
+        props += f'time: {time}, '
+        props += f'length: {length}'
+        props += '}'
+        neo4j.new_relationship(ip_src, ip_dst, 'CONNECTED', relprops=props)
+    
+    def create_ip(self, neo4j, ip):
+        if ip is None:
+            return
+        mc = multicast.ip_multicast(ip)
+        mcast = ''
+        if mc:
+            mcast = ', multicast: true'
+        neo4j.new_node('IP', f'{{name: "{ip}"{mcast}}}')
+    
+    def create_mac(self, neo4j, mac, oui=None):
+        if mac is None:
+            return
+        man = ''
+        if oui not in (None, 'None'):
+            man = f', manufacturer: "{oui}"'
+        multi = ''
+        if multicast.mac_multicast(mac):
+            multi = ', multicast: "likely"'
+        neo4j.new_node('MAC', f'{{name: "{mac}"{man}{multi}}}')
+    
+    def create_mac_assignment(self, neo4j, ip, mac):
+        if mac not in self.ignore and ip is not None:
+            neo4j.new_relationship(ip, mac, 'ASSIGNED')
+    
+    def create_connection(self, neo4j, ip_src, ip_dst, port_dst, proto, time, length, service, service_layer):
+        if port_dst is None:
+            port_dst = -1
+    
+        # Create CONNECTED relationship between IPs
+        query = f'''MATCH (n:IP {{name: "{ip_src}"}})
+    MATCH (m:IP {{name: "{ip_dst}"}})
+    MERGE (n)-[r:CONNECTED {{name: "{port_dst}/{proto}", port: {port_dst}, protocol: "{proto}"}}]->(m)
+        ON CREATE
+            SET r += {{first_seen: {time}, last_seen: {time}, data_size: {length}, service: "{service}", service_layer: {service_layer}, count: 1}}
+        ON MATCH
+            SET r.first_seen = (CASE WHEN {time} > r.first_seen THEN r.first_seen ELSE {time} END)
+            SET r.last_seen = (CASE WHEN {time} < r.last_seen THEN r.last_seen ELSE {time} END)
+            SET r += {{data_size: r.data_size+{length}, count: r.count+1}}
+    return r'''
+        neo4j.raw_query(query)
+        # Update service for CONNECTED relationship
+        query = f'''MATCH (n:IP {{name: "{ip_src}"}})
+    MATCH (m:IP {{name: "{ip_dst}"}})
+    MERGE (n)-[r:CONNECTED {{name: "{port_dst}/{proto}", port: {port_dst}, protocol: "{proto}"}}]->(m)
+        SET r.service = (CASE WHEN {service_layer} > r.service_layer THEN "{service}" ELSE r.service END)
+        SET r.service_layer = (CASE WHEN {service_layer} > r.service_layer THEN "{service_layer}" ELSE r.service_layer END)
+    return r.service'''
+        neo4j.raw_query(query)
+    
+    def create_connection_mac(self, neo4j, mac_src, mac_dst, proto, time, length, service, service_layer):
+        # Create CONNECTED relationship between MACs
+        query = f'''MATCH (n:MAC {{name: "{mac_src}"}})
+    MATCH (m:MAC {{name: "{mac_dst}"}})
+    MERGE (n)-[r:CONNECTED {{name: "{proto}", protocol: "{proto}"}}]->(m)
+        ON CREATE
+            SET r += {{first_seen: {time}, last_seen: {time}, data_size: {length}, service: "{service}", service_layer: {service_layer}, count: 1}}
+        ON MATCH
+            SET r.first_seen = (CASE WHEN {time} > r.first_seen THEN r.first_seen ELSE {time} END)
+            SET r.last_seen = (CASE WHEN {time} < r.last_seen THEN r.last_seen ELSE {time} END)
+            SET r += {{data_size: r.data_size+{length}, count: r.count+1}}
+    return r'''
+        neo4j.raw_query(query)
+        # Update service for CONNECTED relationship
+        query = f'''MATCH (n:MAC {{name: "{mac_src}"}})
+    MATCH (m:MAC {{name: "{mac_dst}"}})
+    MERGE (n)-[r:CONNECTED {{name: "{proto}", protocol: "{proto}"}}]->(m)
+        SET r.service = (CASE WHEN {service_layer} > r.service_layer THEN "{service}" ELSE r.service END)
+        SET r.service_layer = (CASE WHEN {service_layer} > r.service_layer THEN "{service_layer}" ELSE r.service_layer END)
+    return r.service'''
+        neo4j.raw_query(query)
 
-    # Create CONNECTED relationship between IPs
-    query = f'''MATCH (n:IP {{name: "{ip_src}"}})
-MATCH (m:IP {{name: "{ip_dst}"}})
-MERGE (n)-[r:CONNECTED {{name: "{port_dst}/{proto}", port: {port_dst}, protocol: "{proto}"}}]->(m)
-    ON CREATE
-        SET r += {{first_seen: {time}, last_seen: {time}, data_size: {length}, service: "{service}", service_layer: {service_layer}, count: 1}}
-    ON MATCH
-        SET r.first_seen = (CASE WHEN {time} > r.first_seen THEN r.first_seen ELSE {time} END)
-        SET r.last_seen = (CASE WHEN {time} < r.last_seen THEN r.last_seen ELSE {time} END)
-        SET r += {{data_size: r.data_size+{length}, count: r.count+1}}
-return r'''
-    neo4j.raw_query(query)
-    # Update service for CONNECTED relationship
-    query = f'''MATCH (n:IP {{name: "{ip_src}"}})
-MATCH (m:IP {{name: "{ip_dst}"}})
-MERGE (n)-[r:CONNECTED {{name: "{port_dst}/{proto}", port: {port_dst}, protocol: "{proto}"}}]->(m)
-    SET r.service = (CASE WHEN {service_layer} > r.service_layer THEN "{service}" ELSE r.service END)
-    SET r.service_layer = (CASE WHEN {service_layer} > r.service_layer THEN "{service_layer}" ELSE r.service_layer END)
-return r.service'''
-    neo4j.raw_query(query)
-
-def create_connection_mac(neo4j, mac_src, mac_dst, proto, time, length, service, service_layer):
-    # Create CONNECTED relationship between MACs
-    query = f'''MATCH (n:MAC {{name: "{mac_src}"}})
-MATCH (m:MAC {{name: "{mac_dst}"}})
-MERGE (n)-[r:CONNECTED {{name: "{proto}", protocol: "{proto}"}}]->(m)
-    ON CREATE
-        SET r += {{first_seen: {time}, last_seen: {time}, data_size: {length}, service: "{service}", service_layer: {service_layer}, count: 1}}
-    ON MATCH
-        SET r.first_seen = (CASE WHEN {time} > r.first_seen THEN r.first_seen ELSE {time} END)
-        SET r.last_seen = (CASE WHEN {time} < r.last_seen THEN r.last_seen ELSE {time} END)
-        SET r += {{data_size: r.data_size+{length}, count: r.count+1}}
-return r'''
-    neo4j.raw_query(query)
-    # Update service for CONNECTED relationship
-    query = f'''MATCH (n:MAC {{name: "{mac_src}"}})
-MATCH (m:MAC {{name: "{mac_dst}"}})
-MERGE (n)-[r:CONNECTED {{name: "{proto}", protocol: "{proto}"}}]->(m)
-    SET r.service = (CASE WHEN {service_layer} > r.service_layer THEN "{service}" ELSE r.service END)
-    SET r.service_layer = (CASE WHEN {service_layer} > r.service_layer THEN "{service_layer}" ELSE r.service_layer END)
-return r.service'''
-    neo4j.raw_query(query)
-
-'''
-Deprecated, creates too many edges which probably aren't useful anyway
-'''
-def create_port_relationship(neo4j, ip_src, ip_dst, port_src, port_dst, proto, time, length):
-    props = '{'
-    props += f'srcport: {port_src}, '
-    props += f'dstport: {port_dst}, '
-    props += f'protocol: "{proto}", '
-    props += f'time: {time}, '
-    props += f'length: {length}'
-    props += '}'
-    neo4j.new_relationship(ip_src, ip_dst, 'CONNECTED', relprops=props)
-
-def create_mac(neo4j, mac, oui=None):
-    if mac is None:
-        return
-    man = ''
-    if oui not in (None, 'None'):
-        man = f', manufacturer: "{oui}"'
-    multi = ''
-    if multicast.mac_multicast(mac):
-        multi = ', multicast: "likely"'
-    neo4j.new_node('MAC', f'{{name: "{mac}"{man}{multi}}}')
-
-def create_ip(neo4j, ip):
-    if ip is None:
-        return
-    mc = multicast.ip_multicast(ip)
-    mcast = ''
-    if mc:
-        mcast = ', multicast: true'
-    neo4j.new_node('IP', f'{{name: "{ip}"{mcast}}}')
+    def create_ssid(self, neo4j, ssid, mac_src):
+        if ssid is not None:
+            neo4j.new_node('SSID', f'{{name: "{ssid}"}}')
+            neo4j.new_relationship(mac_src, ssid, 'ADVERTISES')
 
 def get_protocol(packet):
     for layer in packet.layers:
